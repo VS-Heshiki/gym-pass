967f9ddfb5bed80fd354830a292c5da60fd3b218
Victor Heshiki <victor_heshiki@hotmail.com>
2023-06-06 18:37:40 -0300

    feat: create authenticate route

--- src/application/routes/routes.ts
+++ src/application/routes/routes.ts
@@ -1,6 +1,8 @@
 import { register } from '@/application/controllers'
+import { auth } from '@/application/controllers'
 import { FastifyInstance } from 'fastify'
 
 export async function appRoutes (app: FastifyInstance): Promise<void> {
     app.post('/users', register)
+    app.post('/sessions', auth)
 }
