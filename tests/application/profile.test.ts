25239cc44b2ec02d3e742f7366a1e300ba60b95f
Victor Heshiki <victor_heshiki@hotmail.com>
2023-06-17 20:41:58 -0300

    test: ensure route GET /me return 200 and user profile on success

---
+++ tests/application/profile.test.ts
@@ -0,0 +1,45 @@
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('POST Register Route', () => {
+
+    beforeAll(async () => {
+        await app.ready()
+    })
+
+    afterAll(async () => {
+        await app.close()
+    })
+
+    it('should be able to get a user profile', async () => {
+        await request(app.server)
+            .post('/user/register')
+            .send({
+                name: 'any_name',
+                email: 'any_email@mail.com',
+                phone: '99999-9999',
+                password: 'any_password'
+            })
+
+        const auth = await request(app.server)
+            .post('/sessions')
+            .send({
+                email: 'any_email@mail.com',
+                password: 'any_password'
+            })
+
+        const response = await request(app.server)
+            .get('/me')
+            .set({
+                Authorization: `Bearer ${auth.body.token}`
+            })
+
+        expect(response.statusCode).toBe(200)
+        expect(response.body).toEqual(expect.objectContaining({
+            name: 'any_name',
+            email: 'any_email@mail.com',
+            phone: '99999-9999'
+        }))
+    })
+})
