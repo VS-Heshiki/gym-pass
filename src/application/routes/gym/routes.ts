cf4177275d9dfe14a106dbea015b0eba7f293277
Victor Heshiki <victor_heshiki@hotmail.com>
2023-06-19 17:59:48 -0300

    feat: add Nearby Gym route

--- src/application/routes/gym/routes.ts
+++ src/application/routes/gym/routes.ts
@@ -1,4 +1,4 @@
-import { registerGym, searchGym } from '@/application/controllers/gym'
+import { listNearbyGym, registerGym, searchGym } from '@/application/controllers/gym'
 import { verifyJWT } from '@/application/middlewares'
 import { FastifyInstance } from 'fastify'
 
@@ -6,5 +6,6 @@ export async function gymRoutes (app: FastifyInstance): Promise<void> {
     app.addHook('onRequest', verifyJWT)
 
     app.post('/gym/register', registerGym)
-    app.post('/gym/search', searchGym)
+    app.get('/gym/search', searchGym)
+    app.get('/gym/near-you', listNearbyGym)
 }
